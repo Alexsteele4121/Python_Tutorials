6eeb9e7edb1aa5105f9edec34392423a63c9715d
Alexsteele4121 <63519706+Alexsteele4121@users.noreply.github.com>
2022-01-04 16:55:37 -0800

    Create Part_1.py

---
+++ Day_1/Part_1.py
@@ -0,0 +1,9 @@
+# PART 1: Different math operators in python.
+
+# Exponent:             **          5 ** 2 = 25
+# Modulus(Remainder):   %           5 % 2 = 1
+# Division:             /           5 / 2 = 2.5
+# Integer Division:     //          5 // 2 = 2
+# Multiplication:       *           5 * 2 = 10
+# Subtraction:          -           5 - 2 = 3
+# Addition:             +           5 + 2 = 7
